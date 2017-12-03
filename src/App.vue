ec7789725f7b684d4132f7f7ccaecf68d76f730f
Joshua Scott <jscott313@gmail.com>
2017-12-03 16:52:07 +0200

    Changed folder structure (added component folder)

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
 </template>
 
 <script>
-  import Header from './Header.vue';
-  import Footer from './Footer.vue';
-  import Servers from './Servers.vue';
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
+  import Servers from './components/Servers.vue';
 
   export default {
     components: {
