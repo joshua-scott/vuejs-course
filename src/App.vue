6c9b5657a970c821b103d6faf869fac04484b81f
Joshua Scott <jscott313@gmail.com>
2017-12-04 17:27:50 +0200

    Setup ready for assignment 7

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <app-user></app-user>
-      </div>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <servers></servers>
+            <app-server-details></app-server-details>
+        </div>
+        <hr>
+        <app-footer></app-footer>
     </div>
-  </div>
 </template>
 
 <script>
-  import User from "./components/User.vue";
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
 
-  export default {
-    components: {
-      appUser: User
+    export default {
+        components: {
+            appHeader: Header,
+            Servers,
+            'app-server-details': ServerDetails,
+            'app-footer': Footer
+        }
     }
-  };
 </script>
 
 <style>
-  div.component {
-    border: 1px solid black;
-    padding: 30px;
-  }
+
 </style>
