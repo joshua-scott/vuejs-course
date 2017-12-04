9bf838ebbc391f41bd50bff7a6bd4274a0bdf923
Joshua Scott <jscott313@gmail.com>
2017-12-04 20:56:43 +0200

    Completed chapter 8

--- src/components/Server/ServerDetails.vue
+++ src/components/Server/ServerDetails.vue
@@ -1,10 +1,34 @@
 <template>
   <div class="col-xs-12 col-sm-6">
-    <p>Server Details are currently not updated</p>
+    <p v-if="!server">Please select a server</p>
+    <p v-else>Server #{{ server.id }} selected, Status {{ server.status }}</p>
+    <hr>
+    <button @click="changeStatus">Change server status</button>
   </div>
 </template>
 
 <script>
+  import { serverBus } from '../../main';
+  export default {
+    data() {
+      return {
+        server: null
+      }
+    },
+    methods: {
+      changeStatus() {
+        // since objects are passed by reference, just this is enough
+        this.server.status = this.server.status === 'Normal' 
+          ? 'Critical'
+          : 'Normal'
+      }
+    },
+    created() {
+      serverBus.$on('serverSelected', (server) => {
+        this.server = server;
+      });
+    }
+  }
 </script>
 
 <style>
--- src/components/Server/Servers.vue
+++ src/components/Server/Servers.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="col-xs-12 col-sm-6">
     <ul class="list-group">
-      <li 
-        class="list-group-item"
-        v-for="index in 5"
-        :key="index">
-          Server #{{ index }}
-      </li>
+      <appServer
+        v-for="server in servers"
+        :server="server"
+        :key="server.id">
+      </appServer>
     </ul>
   </div>
 </template>
 
 <script>
+  import Server from './Server.vue';
+  export default {
+    data() {
+      return {
+        servers: [
+          { id: 1, status: 'Normal' },
+          { id: 2, status: 'Critical' },
+          { id: 3, status: 'Unknown' },
+          { id: 4, status: 'Normal' }
+        ]
+      };
+    },
+    components: {
+      appServer: Server
+    }
+  }
 </script>
 
 <style>
