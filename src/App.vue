01149d8f4697c0daff217d9ffa7e2731b949fc34
Joshua Scott <jscott313@gmail.com>
2017-12-12 19:14:10 +0200

    Learning that slots can have names too!

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@
     <div class="row">
       <div class="col-xs-12">
         <app-quote>
-          <h2>Here we're using Slots</h2>
-          <p>This allows us to put html code *inside* other components... groovy!</p>
-          <p>When using slots:</p>
-          <ul>
-            <li><strong>Styles</strong> from the <strong>child</strong> component are what matters</li>
-            <li>All other data/methods etc are on the <strong>parent</strong> component</li>
-          </ul>
+          <h2 slot="title">Here we're using Slots</h2>
+          <div slot="content">
+            <p>This allows us to put html code *inside* other components... groovy!</p>
+            <p>When using slots:</p>
+            <ul>
+              <li><strong>Styles</strong> from the <strong>child</strong> component are what matters</li>
+              <li>All other data/methods etc are on the <strong>parent</strong> component</li>
+            </ul>
+          </div>
         </app-quote>
       </div>
     </div>
