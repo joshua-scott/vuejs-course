09bb5171a1aa2a25dac4029ad98408ba8d199582
Joshua Scott <jscott313@gmail.com>
2017-12-12 17:48:52 +0200

    Chapter 9 setup complete

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-    <div class="container">
-        <app-header></app-header>
-        <hr>
-        <div class="row">
-            <servers></servers>
-            <app-server-details></app-server-details>
-        </div>
-        <hr>
-        <app-footer></app-footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import Header from './components/Shared/Header.vue';
-    import Footer from './components/Shared/Footer.vue';
-    import Servers from './components/Server/Servers.vue';
-    import ServerDetails from './components/Server/ServerDetails.vue';
+  import Quote from './components/Quote.vue'
 
-    export default {
-        components: {
-            appHeader: Header,
-            Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
+  export default {
+    components: {
+      appQuote: Quote
     }
+  }
 </script>
 
 <style>
