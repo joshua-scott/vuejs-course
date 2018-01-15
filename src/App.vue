13363387ed0841e84b6ce77ad770b09250849499
Joshua Scott <jscott313@gmail.com>
2018-01-15 15:59:18 +0200

    About to begin assignment 8: slots and dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,33 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <app-quote>
-          <h2 slot="title">Here we're using Slots</h2>
-          <span slot="subtitle" class="subtitle">There is indeed text here, so this is what's shown. But if this wasn't passed down, the fallback text (from the Quote component itself) would be displayed.</span>
-          <div>
-            <p>This allows us to put html code *inside* other components... groovy!</p>
-            <p>When using slots:</p>
-            <ul>
-              <li><strong>Styles</strong> from the <strong>child</strong> component are what matters</li>
-              <li>All other data/methods etc are on the <strong>parent</strong> component</li>
-            </ul>
-          </div>
-        </app-quote>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary">Load Blue Template</button>
+                <button class="btn btn-success">Load Green Template</button>
+                <button class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <app-blue></app-blue>
+                <app-green></app-green>
+                <app-red></app-red>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-  import Quote from './components/Quote.vue'
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
 
-  export default {
-    components: {
-      appQuote: Quote
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
     }
-  }
 </script>
 
 <style>
-  li {
-    list-style: none;
-  }
-
-  .subtitle {
-    color: #ccc;
-  }
 </style>
