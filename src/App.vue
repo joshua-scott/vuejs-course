6c06cb8fe240e87ae991c71e0dc81d1f7b100b63
Joshua Scott <jscott313@gmail.com>
2018-01-17 19:30:50 +0200

    Halfway through the Wonderful Quotes project

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,49 @@
 <template>
     <div class="container">
-    
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <div class="alert alert-info">Simply click on a quote to delete it.</div>
+          </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+
+export default {
+  data: function() {
+    return {
+      quotes: [
+        "Just a quote to see something",
+        "and another",
+        "striker!!!",
+        "foot like a traction engine",
+        "shit!!!",
+        "did ya see that??!!"
+      ],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    newQuote(text) {
+      if (this.quotes.length + 1 <= this.maxQuotes) {
+        this.quotes.push(text);
+      } else {
+        alert(`You already have ${this.maxQuotes} quotes!`);
+      }
     }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
+  }
+};
 </script>
 
 <style>
+
 </style>
