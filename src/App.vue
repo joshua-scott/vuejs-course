14bdb00f0fbadf4d22f0d10c6b78c4a5cbe701b1
Joshua Scott <jscott313@gmail.com>
2018-01-18 19:36:54 +0200

    Setup ready for chapter 11 (forms)

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,127 @@
 <template>
-  <div class="container">
-      <app-progress-bar :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-progress-bar>
-      <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-      <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
-      <div class="row">
-        <div class="col-sm-12 text-center">
-          <div class="alert alert-info">Simply click on a quote to delete it.</div>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <h1>File a Complaint</h1>
+                    <hr>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="age">Age</label>
+                        <input
+                                type="number"
+                                id="age"
+                                class="form-control">
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="message">Message</label><br>
+                    <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
+                    <textarea
+                            id="message"
+                            rows="5"
+                            class="form-control"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <div class="form-group">
+                        <label for="sendmail">
+                            <input
+                                    type="checkbox"
+                                    id="sendmail"
+                                    value="SendMail"> Send Mail
+                        </label>
+                        <label for="sendInfomail">
+                            <input
+                                    type="checkbox"
+                                    id="sendInfomail"
+                                    value="SendInfoMail"> Send Infomail
+                        </label>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                    <label for="male">
+                        <input
+                                type="radio"
+                                id="male"
+                                value="Male"> Male
+                    </label>
+                    <label for="female">
+                        <input
+                                type="radio"
+                                id="female"
+                                value="Female"> Female
+                    </label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+                    <label for="priority">Priority</label>
+                    <select
+                            id="priority"
+                            class="form-control">
+                        <option></option>
+                    </select>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary">Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Mail:</p>
+                        <p>Password:</p>
+                        <p>Age:</p>
+                        <p>Message: </p>
+                        <p><strong>Send Mail?</strong></p>
+                        <ul>
+                            <li></li>
+                        </ul>
+                        <p>Gender:</p>
+                        <p>Priority:</p>
+                        <p>Switched:</p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-  </div>
+    </div>
 </template>
 
 <script>
-import QuoteGrid from './components/QuoteGrid.vue'
-import NewQuote from './components/NewQuote.vue'
-import ProgressBar from './components/ProgressBar.vue'
-
-export default {
-  data: function() {
-    return {
-      quotes: [
-        "Just a quote to see something",
-        "and another",
-        "striker!!!",
-        "foot like a traction engine",
-        "shit!!!",
-        "did ya see that??!!"
-      ],
-      maxQuotes: 10
-    };
-  },
-  methods: {
-    newQuote(text) {
-      if (this.quotes.length < this.maxQuotes) {
-        this.quotes.push(text)
-      } else {
-        alert(`You already have ${this.maxQuotes} quotes!`)
-      }
-    },
-    deleteQuote(index) {
-      this.quotes.splice(index, 1)
+    export default {
     }
-  },
-  // calculated: {
-  //   progress() {
-  //     return (quotes / maxQuotes) * 100;
-  //   }
-  // },
-  components: {
-    appQuoteGrid: QuoteGrid,
-    appNewQuote: NewQuote,
-    appProgressBar: ProgressBar
-  }
-};
 </script>
 
 <style>
