323c1feeacebc3c251e0fc934ce61222031a9bc4
Joshua Scott <jscott313@gmail.com>
2018-02-06 19:32:13 +0200

    Completed chapter 12 (directives)

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,34 @@
-<template>
-    <div class="container">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
-
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
-                </div>
-            </div>
-        </form>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<template lang="pug">
+  div(class="container")
+    div(class="row")
+      div(class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3")
+        h2 Built-in Directives
+        p(v-text="'v-text outputs text'")
+        p(v-html="'<strong>v-html</strong> parses html'")
+    div(class="row")
+      div(class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3")
+        h2 Custom Directives
+        p(v-highlight:background="'yellow'") Color this (global directive)
+        p(v-customOn:mouseout="doTheThing") 'v-on' reimplemented using a local custom directive
 </template>
 
 <script>
-    export default {
+  export default {
+    methods: {
+      doTheThing () {
+        alert('Hello')
+      }
+    },
+    directives: {
+      customOn: {
+        inserted (el, binding) {
+          el.addEventListener(binding.arg, binding.value)
+        }
+      }
     }
+  }
 </script>
 
 <style>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('highlight', {
+  bind (el, binding, vnode) {
+    const delay = binding.modifiers.delayed ? 3000 : 0
+
+    setTimeout(() => {
+      if (binding.arg === 'background') {
+        el.style.backgroundColor = binding.value
+      } else {
+        el.style.color = binding.value
+      }
+    }, delay);
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
